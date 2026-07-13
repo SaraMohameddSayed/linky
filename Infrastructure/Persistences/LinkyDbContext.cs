28a850c31cd69dbb4519ab6662da586c14b95dca
Sara Mohamed <saramohamedsci884@gmail.com>
2026-07-14 01:07:18 +0300

    Initialize .NET 8.0 solution with clean architecture
    Set up a new .NET 8.0 solution with four projects: Api, Application, Infrastructure, and Domain.
    
    - Added `Api.csproj` with references to essential NuGet packages (e.g., EntityFrameworkCore, Serilog, Swashbuckle) and project dependencies.
    - Configured `Program.cs` with services for controllers, CORS, Swagger, and EF Core with SQL Server.
    - Added `launchSettings.json` and `appsettings.json` for environment configuration, logging, and connection strings.
    - Created `Infrastructure` project with EF Core setup, including `LinkyDbContext` and initial migration files.
    - Defined solution structure in `Linky.sln` for maintainability and scalability.

---
+++ Infrastructure/Persistences/LinkyDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Persistences
+{
+    public class LinkyDbContext:DbContext
+    {
+        public LinkyDbContext(DbContextOptions<LinkyDbContext> options) : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
